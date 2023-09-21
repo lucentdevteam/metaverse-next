e0e8323f951b658e2d1ed8453d7c50b3410aa9bd
akhileshpatidar001 <akhilesh.patidar@lucentinnovation.co.in>
2023-09-21 13:53:33 +0530

    created vercel.json file with rewrite

--- express/index.js
+++ express/index.js
@@ -1,25 +1,30 @@
 const express = require("express");
-const CONFIG = require("./config")
+const CONFIG = require("./config");
 const cors = require("cors");
-const PORT = CONFIG.PORT || 5000
+const PORT = CONFIG.PORT || 5000;
 const { exceptionHandling } = require("./middleware/errors/exceptionHandling");
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 let db = require("./models/index.js");
 
-db.sequelize.sync({ alter: true }).then(() => {
-  console.log("Mysql Database Connected Successfully");
-}).catch(error => {
-  console.log("Error to connect mysql database", error);
-})
+db.sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Mysql Database Connected Successfully");
+  })
+  .catch((error) => {
+    console.log("Error to connect mysql database", error);
+  });
 
 require("./routes/user-route")(app);
 
 //error handling
-app.use(exceptionHandling)
+app.use(exceptionHandling);
 
 app.listen(PORT, () => {
   console.log(`APP LISTENING ON PORT ${PORT}`);
-})
+});
+
+export default app;
